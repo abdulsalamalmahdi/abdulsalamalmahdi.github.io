e578729ce7fb98af4d5ae2cbf9b9d90a90eed042
root <root@DESKTOP-UH6ABS8.localdomain>
2021-01-05 16:33:19 +0100

    fixing gh-pages routing proplem

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/https://abdulsalamalmahdi.github.io/'
+    : '/',
   // "devServer": {
   //   "https": false
   // },
